5e49be04b4f2dd914801073366bfa1835733fdb3
AlexandrIvanov <alexandr.1996@list.ru>
2023-09-21 18:45:12 +0300

    First test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+    const [value, setValue] = useState('');
+    const onChangeValue = (e) => {
+        setValue(e.target.value)
+    }
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Search value={value} onChange={onChangeValue}>Search:</Search>
+            <span>Search for: {value ? value : '...'}</span>
         </div>
     );
+}
+
+export const Search = ({children, value, onChange}) => {
+    return (
+        <div>
+            <label htmlFor="search">{children}</label>
+            <input
+                placeholder='Search...'
+                type="text"
+                id="search"
+                value={value}
+                onChange={onChange}
+            />
+            <img src="" alt="img"/>
+        </div>
+    )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-    render(<App/>);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+    it('renders App component', () => {
+        render(<App/>)
+        expect(screen.getByText(/search:/i)).toBeInTheDocument();
+        expect(screen.getByRole('textbox')).toBeInTheDocument();
+        expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
+        expect(screen.getByPlaceholderText(/search.../i)).toBeInTheDocument();
+        expect(screen.getByAltText('img')).toBeInTheDocument();
+        expect(screen.getByDisplayValue('')).toBeInTheDocument();
+    });
+})
