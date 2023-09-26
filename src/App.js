daf81c4feea1861da65ef6588c7d16239bd36292
AlexandrIvanov <alexandr.1996@list.ru>
2023-09-26 13:36:47 +0300

    Use getBy, queryBy and findBy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+
+const getUser = () => Promise.resolve({id: 1, name: 'Alex'})
 
 function App() {
     const [value, setValue] = useState('');
+    const [user, setUser] = useState()
+
+    useEffect(() => {
+        const loadUser = async () => {
+            const user = await getUser()
+            setUser(user)
+        }
+        loadUser()
+    }, []);
+
     const onChangeValue = (e) => {
         setValue(e.target.value)
     }
     return (
         <div className="App">
+            {user && <h2>Username {user.name}</h2>}
             <Search value={value} onChange={onChangeValue}>Search:</Search>
             <span>Search for: {value ? value : '...'}</span>
         </div>
@@ -24,6 +37,7 @@ export const Search = ({children, value, onChange}) => {
                 id="search"
                 value={value}
                 onChange={onChange}
+                // required
             />
             <img src="" alt="img"/>
         </div>
--- src/App.test.js
+++ src/App.test.js
@@ -4,11 +4,34 @@ import App from './App';
 describe('App', () => {
     it('renders App component', () => {
         render(<App/>)
-        expect(screen.getByText(/search:/i)).toBeInTheDocument();
+
+        // If you need to find an element - getBy
+        expect(screen.getByText(/search for/i)).toBeInTheDocument();
         expect(screen.getByRole('textbox')).toBeInTheDocument();
         expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
         expect(screen.getByPlaceholderText(/search.../i)).toBeInTheDocument();
         expect(screen.getByAltText('img')).toBeInTheDocument();
         expect(screen.getByDisplayValue('')).toBeInTheDocument();
+    });
+    it('renders App component with queryBy', () => {
+        render(<App/>)
+
+        // If you need to show, that an element missing in the DOM - queryBy
+        expect(screen.queryByText(/search for: react/i)).toBeNull();
+    });
+    it('renders App component with findBy', async () => {
+        render(<App/>)
+        expect(screen.queryByText(/Username/i)).toBeNull();
+
+        // If the element appears when the asynchronous code is running
+        expect(await screen.findByText(/Username alex/i)).toBeInTheDocument();
+    });
+    it('Input test', () => {
+        render(<App/>);
+        const input = screen.getByRole('textbox');
+
+        expect(input).not.toBeRequired();
+        expect(input).toBeEmptyDOMElement();
+        expect(input).toHaveAttribute('id');
     });
 })
